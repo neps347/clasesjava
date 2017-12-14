79a983aecc156b09d3b575b3097f055a5bde33c0
Baudet <e-baudet@hotmail.es>
2017-12-14 20:47:35 +0000

    Commit inicial

---
+++ src/hola.java
@@ -0,0 +1,12 @@
+
+public class hola {
+
+	public static void main(String[] args) {
+		System.out.println("¡Bienvenido al sistema!\r\n");
+		System.out.println("Hoy es Jueves");
+		System.out.println("Son las 20:10 Horas. Hora Canaria");
+		System.out.println("Tiene 1600 correos sin leer");
+	}
+
+}
+
