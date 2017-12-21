d845f7cbd7cc8633257a4cf2947336e5335a8a0a
neps347 <33439042+neps347@users.noreply.github.com>
2017-12-21 17:07:08 +0000

    Update hola.java
    correcion mayuscula? revisar

--- src/hola.java
+++ src/hola.java
@@ -1,8 +1,8 @@
-
+/* añador mayuscula "public class Hola"? */
 public class hola {
 
 	public static void main(String[] args) {
-		System.out.println("�Bienvenido al sistema!\r\n");
+		System.out.println("¡Bienvenido al sistema!\r\n");
 		System.out.println("Hoy es Jueves");
 		System.out.println("Son las 20:10 Horas. Hora Canaria");
 		System.out.println("Tiene 1600 correos sin leer");
